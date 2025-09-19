b99859ceb965f19b1d38e9a83f8c9b1e13c48084
Rouhin Banerjee <{email}>
2025-09-19 11:14:03 +0530

    19-9-25

--- src/app/deliveryagent/page.tsx
+++ src/app/deliveryagent/page.tsx
@@ -65,23 +65,23 @@ export default function DeliveryAgentPage() {
     }
   };
 
-  const handleDownload = async (url: string, filename: string) => {
-    try {
-      const res = await fetch(url);
-      const blob = await res.blob();
-      const blobUrl = window.URL.createObjectURL(blob);
+  // const handleDownload = async (url: string, filename: string) => {
+  //   try {
+  //     const res = await fetch(url);
+  //     const blob = await res.blob();
+  //     const blobUrl = window.URL.createObjectURL(blob);
 
-      const link = document.createElement("a");
-      link.href = blobUrl;
-      link.download = filename;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(blobUrl);
-    } catch  {
-      toast.error("Failed to download image");
-    }
-  };
+  //     const link = document.createElement("a");
+  //     link.href = blobUrl;
+  //     link.download = filename;
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //     window.URL.revokeObjectURL(blobUrl);
+  //   } catch  {
+  //     toast.error("Failed to download image");
+  //   }
+  // };
 
   return (
     <SidebarProvider>
