fa33a7fdb3a86073cace11c147d2613800f780ee
Rouhin Banerjee <{email}>
2025-09-22 19:55:09 +0530

    22-9-25

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,11 +5,12 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-screen lg:grid-cols-2">
-      {/* Left image panel */}
-      <div className="relative hidden bg-muted lg:block">
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-12">
+      
+      {/* Left image panel - 7/12 width */}
+      <div className="relative hidden lg:col-span-7 bg-muted lg:block">
         <Image
-          src="/home/image (3).jpeg"
+          src="/login.png"
           alt="Login background"
           fill
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
@@ -21,20 +22,25 @@ export default function LoginPage() {
         </div>
       </div>
 
-      {/* Right form panel */}
-      <div className="flex flex-col justify-center p-8 md:p-16">
+      {/* Right form panel - 5/12 width */}
+      <div className="lg:col-span-5 flex flex-col justify-center p-8 md:p-16">
         <div className="mb-8 text-center md:text-center">
-          <h2 className="text-3xl font-semibold ">Login</h2>
+          <Image
+            src="/favicon.jpeg"
+            alt="Logo"
+            height={50}
+            width={50}
+            className="mx-auto mb-2 rounded-full"
+          />
+          <h2 className="text-3xl font-bold text-[#006b3d]">CALCUTTA FRESH FOODS</h2>
           <p className="text-sm text-muted-foreground mt-1">
-            Enter your credentials to access your dashboard
+            Login with your credentials to access Hub Manager and Admin Panels
           </p>
         </div>
 
         <div className="w-full max-w-md mx-auto">
           <LoginForm />
         </div>
-
-        
       </div>
     </div>
   );
